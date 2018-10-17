ec15a0df42460b7c852c94eb13891bee6aa713f8
Fernando García López <furia2010598@hotmail.com>
2018-10-16 20:46:28 -0500

    Validacion de logueo
    Se modificó la consulta de mysql para validar el acceso al sistema

--- validar.php
+++ validar.php
@@ -1,47 +1,15 @@
 <?php
 @session_start();
-require("connect_db.php");
+require("MyConexion.php");
 
 $usuario=$_POST['usuario'];
 $pass=$_POST['pass'];
-
-
-
-	$sql=mysql_query("SELECT * FROM login WHERE usuario='$usuario'");
-	
-
-	if($r=mysql_fetch_array($sql)){
-		
-
-
-		if($pass==$r['pass']){
-			$_SESSION['usuario']=$r['usuario'];
-			$_SESSION['pass']=$r['pass'];
-			
-
-			
-			echo "<script>location.href='menu.html'</script>";
-		
-		}
-else{
-		echo '<script>alert("su nombre de usuario o password es invalido, verifique de nuevo")</script> ';
-        echo "<script>location.href='login.html'</script>";
-    }
-		
-	}
-
-
-
-	else{
-		echo '<script>alert("su nombre de usuario o password es invalido, verifique de nuevo")</script> ';
-        echo "<script>location.href='login.html'</script>";
-    }
-
-
-
-
-
-
-
-
-?>
+if($sql=mysql_query("SELECT * FROM login WHERE usuario='$usuario' AND pass='$pass'")){
+	$r=mysql_fetch_assoc($sql);
+	$_SESSION['usuario']=$r['usuario'];
+	$_SESSION['pass']=$r['pass'];
+	echo "<script>location.href='menu.html'</script>";
+}else{
+	echo '<script>alert("su nombre de usuario o password es invalido, verifique de nuevo")</script> ';
+    echo "<script>location.href='login.html'</script>";
+}
